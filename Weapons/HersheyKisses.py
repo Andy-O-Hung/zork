1493159c446e407fd5ef204038e3b5a526de688e
andy-o-hung <hunga@dc19.cis.gvsu.edu>
2018-03-14 08:50:58 -0400

    Weapn doenst work..

---
+++ Weapons/HersheyKisses.py
@@ -0,0 +1,17 @@
+from random import *
+
+def HersheyKisses(object):
+	
+	def __init__(self):
+		self.name = "HersheyKisses"
+		self.amount = 10000
+		self.mod = 1
+
+	def getName(self):
+		return self.name
+
+	def getAmount(self):
+		return self.amount
+
+	def getMod(self):
+		return self.mod
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from Npc import *
-
+from Weapons import *
 def main():
 	print("hello")
 	test =  Person()
 	print(test.asdf())
 	print(test.getHealthPoints())
-
+	testNpc = Npc()
+	print(testNpc.getHealthPoints())
+	testWeapon = HersheyKisses.HersheyKisses()
+	print(testWeapon.getAmount())
 
 if __name__ == "__main__":
 	main()
