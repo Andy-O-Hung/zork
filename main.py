a37bae2d61714deecd0a5af0e202d872fd0c8836
Javier Ramirez <ramijavi@mail.gvsu.edu>
2018-04-01 22:00:18 -0400

    Added coments
    I know this is way after the deadline of the project, but I just wanted to add a bit more clarification to the code.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+# Import the necessary files 
+# Note that not all classes are imported. This is because only the necessary ones are imported 
+# into the main, and then the other ones are imported into the other classes when necessary
 from Player import *
 from Neighborhood import *
 from Game import *
 
-#The main method of the game.
+# The main method of the game.
 def main():
 
+	# Basic welcome message to greet the user
 	print "Welcome to ZORK"
 
-	#Calls the game class to start the game.
+	# Creates an instance of the game class, and calls its run function
 	currGame = Game()
 	currGame.run()
-
-
 
 #This checks if this file is named main.
 if __name__ == "__main__":
